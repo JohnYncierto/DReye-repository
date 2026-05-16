f824f0cbe246cdc750850b96ed583921ceb85b1c
JohnYncierto <jhyncierto@gmail.com>
2026-05-16 14:15:48 +0800

    Upload box update

--- frontend/src/components/UploadBox.jsx
+++ frontend/src/components/UploadBox.jsx
@@ -151,7 +151,6 @@ export default function UploadBox() {
                 <button
                     onClick={handleSubmit}
                     disabled={loading}
-                    type="submit"
                     className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
                 >
                     {loading ? 'Saving' : 'Save Result'}
